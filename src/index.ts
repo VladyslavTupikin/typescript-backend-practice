4b75d62487457b4a4be581193aea167129898581
Vladyslav Tupikin <vladtupikin7@gmail.com>
2026-08-24 23:35:03 +0300

    index.ts: Add default values in case .env does not exist

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Application } from "./app.js";
-import sqlite3, { PROTOCOL } from "sqlite3";
+import sqlite3 from "sqlite3";
 import process from "node:process";
 
 try {
@@ -8,9 +8,9 @@ try {
   console.info("Could not load .env file, continue with default parameters.");
 }
 
-const HOST = process.env.HOST;
-const PORT = Number(process.env.PORT);
-const CLIENT_PORT = Number(process.env.CLIENT_PORT);
+const HOST = process.env.HOST ?? "127.0.0.1";
+const PORT = Number(process.env.PORT ?? 5000);
+const CLIENT_PORT = Number(process.env.CLIENT_PORT ?? 5000);
 
 const db = new sqlite3.Database(":memory:");
 const app = new Application(db, HOST, PORT, CLIENT_PORT);
