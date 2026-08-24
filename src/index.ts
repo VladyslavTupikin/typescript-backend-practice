70e5db68f9d14bcdfe35ae7d80b53c0ad3354b24
Vladyslav Tupikin <vladtupikin7@gmail.com>
2026-08-25 00:16:01 +0300

    index.ts: Add work with external sqlite3 database

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,13 @@ const HOST = process.env.HOST ?? "127.0.0.1";
 const PORT = Number(process.env.PORT ?? 5000);
 const CLIENT_PORT = Number(process.env.CLIENT_PORT ?? 5000);
 
-const db = new sqlite3.Database(":memory:");
+const dbPath = "./products.db";
+const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    throw Error(`Failed to load database ${dbPath}, error: ${err}. Exiting...`);
+  }
+});
+
 const app = new Application(db, HOST, PORT, CLIENT_PORT);
 
 app.Run();
