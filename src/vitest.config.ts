9930189ea8359d34b34e0d702af8b722bcafde5f
Vladyslav Tupikin <vladtupikin7@gmail.com>
2026-08-26 21:14:31 +0300

    unit-tests: Add configuration for vitest

---
+++ src/vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+    // Optional: Enables static type testing via `tsc` or `vue-tsc`
+    typecheck: {
+      enabled: true,
+    },
+    coverage: {
+      provider: "v8", // Default mechanism
+      reporter: ["text", "lcov", "html"], // Formats generated from V8 data
+    },
+  },
+});
